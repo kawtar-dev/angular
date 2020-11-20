a0876e2ed00ddf52de4409c6de7feb70e796cff2
Kawtar <benzayer.kawtar@gmail.com>
2020-11-20 13:14:40 +0100

    my first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FusionChartsModule} from 'angular-fusioncharts';
 
+import * as FusionCharts from 'fusioncharts';
+import * as charts from 'fusioncharts/fusioncharts.charts';
+import * as FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+
+FusionChartsModule.fcRoot(FusionCharts, charts, FusionTheme);
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FusionChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
